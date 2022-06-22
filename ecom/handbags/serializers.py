8cb9d5e868445a26f30b39b53b864adb0d48ed37
Buyaki01 <rittahbuyaki@gmail.com>
2022-06-22 19:40:15 +0300

    Install the djangorestframework and django-cors-headers and serializers

---
+++ ecom/handbags/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from models import (Category,
+        Orders,
+        Products,
+        User, 
+        )
+
+class CategorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Category
+    fields = '__all__'
+
+class OrdersSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Orders
+    fields = '__all__'
+
+class ProductsSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Products
+    fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = '__all__'

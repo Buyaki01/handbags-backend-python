d8b452c38df5b6fda5be6b8609ebe487bcd61a55
Buyaki01 <rittahbuyaki@gmail.com>
2022-06-22 19:25:31 +0300

    Add views/home.py

---
+++ ecom/handbags/views/home.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from handbags.models.products import Products
+from handbags.models.category import Category
+from django.views import View
+  
+  
+# Create your views here.
+
